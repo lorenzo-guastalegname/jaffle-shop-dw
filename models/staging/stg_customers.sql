dc74820bbca4b530b66ed3185698aece3a116947
Lorenzo <guastalegname64@gmail.com>
2026-06-28 15:00:13 +0200

    feat: initial jaffle_shop project setup

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,16 +1,8 @@
 ﻿-- Staging: clean and standardise raw customers.
--- feat(TICK-101): add full_name and customer_tier for CRM segmentation project
 select
     customer_id,
     first_name,
     last_name,
-    lower(email)                                   as email,
-    concat(first_name, ' ', last_name)             as full_name,
-    case
-        when lifetime_orders >= 10 then 'platinum'
-        when lifetime_orders >= 5  then 'gold'
-        when lifetime_orders >= 1  then 'silver'
-        else                            'new'
-    end                                            as customer_tier
+    lower(email) as email
 from {{ source('raw', 'customers') }}
 where customer_id is not null
